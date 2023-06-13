e82379fb606afa5a793d42be16fe7f15e03629a0
Vincent <makokhavomondi@gmail.com>
2023-06-13 11:20:43 +0300

    Update score.py to use np.sum to create scores by default

--- score.py
+++ score.py
@@ -1,14 +1,7 @@
 from stocksymbol import StockSymbol
 from scraper import scrape_financial_info
-from ssk import ssk
 import pandas as pd
 import numpy as np
-
-ssm = StockSymbol(ssk)
-
-# Get tickers of companies listed in NASDAQ
-symbol_list = ssm.get_symbol_list(market="US")
-nasdaq_list = [x['symbol'] for x in symbol_list if x['exchange'] == 'NASDAQ']
 
 # Get some of the important financial ratios
 def get_ratios(ticker):
@@ -17,7 +10,7 @@ def get_ratios(ticker):
     financial_info['ebtda-margin'] = financial_info['ebitda'].astype(float) / financial_info['revenue'].astype(float)
     financial_info['gross-profit-margin'] = financial_info['gross-profit'].astype(float) / financial_info['revenue'].astype(float)
     financial_info['cogs-margin'] = financial_info['cost-goods-sold'].astype(float) / financial_info['revenue'].astype(float)
-    financial_info['eps-basic-net-earnings-per-share'] = financial_info['eps-basic-net-earnings-per-share'].astype(float)
+    # financial_info['eps-basic-net-earnings-per-share'] = financial_info['eps-basic-net-earnings-per-share'].astype(float)
     financial_info['eps-earnings-per-share-diluted'] = financial_info['eps-earnings-per-share-diluted'].astype(float)
 
     return financial_info[
@@ -25,7 +18,7 @@ def get_ratios(ticker):
                 'ebtda-margin',
                 'gross-profit-margin',
                 'cogs-margin',
-                'eps-basic-net-earnings-per-share',
+                # 'eps-basic-net-earnings-per-share',
                 'eps-earnings-per-share-diluted' 
             ]]
 
@@ -40,13 +33,23 @@ def normalize(df):
 
     return pd.DataFrame(result)
 
-# Calculate the score by calculating the product of all normalized
-# value of the fundamental variable at each time
-def get_score(ticker):
+# Calculate the score
+def get_score(ticker, method=np.sum):
     ratios = normalize(get_ratios(ticker))
-    score = ratios.apply(lambda row: np.prod(row), axis=1)
+    score = ratios.apply(lambda row: method(row), axis=1)
+
+    # Pick only the year for index
+    score = score.reset_index()
+    score['year'] = score['index'].apply(lambda x: int(x.split("-")[0]))
+    score = score.set_index('year')
+
+    # Convert score to dataframe and use ticker name as column name
+    score = pd.DataFrame(score)[[0]]
+    score.rename(columns={0: ticker}, inplace=True)
 
     # Adjust score such that the first value = 1
-    first_val = score[0]
-    return score / first_val
+    first_year_raw_score = score.sort_index(ascending=True).iloc[0]
+    score[ticker] = score[ticker].apply(lambda x: x/first_year_raw_score)
+
+    return score
     
