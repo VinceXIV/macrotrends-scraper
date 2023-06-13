5d57f1375ff89cacb3b3c07f9e4561b7272509b9
Vincent <makokhavomondi@gmail.com>
2023-06-13 12:33:11 +0300

    Sort score dates from smallest to largest

--- score.py
+++ score.py
@@ -51,6 +51,7 @@ def get_score(ticker, method=np.sum):
     first_year_raw_score = score.sort_index(ascending=True).iloc[0]
     score[ticker] = score[ticker].apply(lambda x: x/first_year_raw_score)
 
+    score = score.sort_index(ascending=True)
     return score
     
 
