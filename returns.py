0ab5d573a15d6333ae28a5459b9ec2c8d1f77186
Vincent <makokhavomondi@gmail.com>
2023-06-13 14:06:40 +0300

    Ignore tickers that fail to download

--- returns.py
+++ returns.py
@@ -61,31 +61,32 @@ def get_monthly_return(ticker_list, start_year):
 # Where the first price is set to 1
 def get_monthly_adjusted_price(ticker_list, start_year):
     result = {}
+
     for ticker in ticker_list:
-        data = yf.download(ticker, progress=False).reset_index()
-        data = data[data['Date'].dt.year >= start_year]
-        data['year'] = data['Date'].dt.year
-        data['month'] = data['Date'].dt.month
+        try:
+            data = yf.download(ticker, progress=False).reset_index()
+            data = data[data['Date'].dt.year >= start_year]
+            data['year'] = data['Date'].dt.year
+            data['month'] = data['Date'].dt.month
 
-        start_month_price = None
+            start_month_price = None
 
-        result[ticker] = {}
-        for year in data['year'].unique():
-            try:
-                yearly_data = data[data['year'] == year]
+            result[ticker] = {}
+            for year in data['year'].unique():
+                    yearly_data = data[data['year'] == year]
 
-                for month in data['month'].unique():
-                    monthly_data = yearly_data[yearly_data['month'] == month]
-                    end_date = monthly_data['Date'].max()
-                    end_adj_close = float(data[data['Date'] == end_date]['Adj Close'])
+                    for month in data['month'].unique():
+                        monthly_data = yearly_data[yearly_data['month'] == month]
+                        end_date = monthly_data['Date'].max()
+                        end_adj_close = float(data[data['Date'] == end_date]['Adj Close'])
 
-                    if start_month_price == None:
-                        start_month_price = end_adj_close
+                        if start_month_price == None:
+                            start_month_price = end_adj_close
 
-                    adj_price = end_adj_close/start_month_price
+                        adj_price = end_adj_close/start_month_price
 
-                    result[ticker]["{y}-{m}".format(y=year, m=month)] = adj_price
-            except:
-                continue   
+                        result[ticker]["{y}-{m}".format(y=year, m=month)] = adj_price  
+        except:
+            continue
 
     return pd.DataFrame(result)
