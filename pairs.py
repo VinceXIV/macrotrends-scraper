2aec10ded89b986f5934ed9db4cdea6ddf5d5fea
Vincent <makokhavomondi@gmail.com>
2023-07-16 18:55:26 +0300

    Enable both multiply normalized fundamental variables before calculating ssd, doing the multiplication after calculating ssd

--- pairs.py
+++ pairs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import copy
 
 def get_pair_distance(df, start_year_month=None, end_year_month=None):
     '''
@@ -14,6 +15,8 @@ def get_pair_distance(df, start_year_month=None, end_year_month=None):
     df = df.dropna(axis=1)
 
     df_ssd = {}
+
+    # calculate the sum of squared difference for each column
     for stock_1 in df:
         df_ssd[stock_1] = {}
         for stock_2 in df:
@@ -35,3 +38,25 @@ def get_pair_distance(df, start_year_month=None, end_year_month=None):
                 pairs[key] = df_ssd[row][col]
 
     return pd.Series(pairs).sort_values(ascending=False)
+
+def get_distance(df1, df2_list = [], start_year_month=None, end_year_month=None):
+    # because we don't want to alter the original df1 sent in this function
+    df = copy.deepcopy(df1)
+    
+    # because if column has all values as NaN, multiplying by others will result
+    # in all of them having NaN
+    df = df.dropna(axis=1, how='all')
+
+    # We expect df1 and df2 to have the same column names with the content
+    # being for different variables. For instance, one df will have information
+    # about normalized ebtda-margin, while another will have information about
+    # normalized cost of goods sold
+    for df2 in df2_list:
+        df2 = df2.dropna(axis=1, how='all')
+        for column in df1:
+            try:
+                df[column] = df[column] * df2[column]
+            except:
+                continue
+
+    return get_pair_distance(df, start_year_month, end_year_month)
