10d7782eac8241c5e7ff33199fd7f1458bc74686
Vincent <makokhavomondi@gmail.com>
2023-07-16 20:52:23 +0300

    Create method for returning normalized yearly data

--- returns.py
+++ returns.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import yfinance as yf
-
+import numpy as np
 
 def get_yearly_return(ticker_list, start_year):
     result = {}
@@ -97,3 +97,29 @@ def get_monthly_adjusted_price(ticker_list, start_year):
     result = result.set_index(["year", "month"])
 
     return result
+
+
+def get_yearly_normalized_price(ticker_list, start_year):
+    result = {}
+    for ticker in ticker_list:
+        data = yf.download(ticker, progress=False, show_errors=False).reset_index()
+        data = data[data['Date'].dt.year >= start_year]
+        data['year'] = data['Date'].dt.year
+
+        result[ticker] = {}
+        max_price = data['Adj Close'].max()
+        min_price = data['Adj Close'].min()
+        normalization_factor = max_price - min_price
+        for year in data['year'].unique():
+            try:
+                yearly_data = data[data['year'] == year]
+                end_date = yearly_data['Date'].max()
+
+                end_adj_close = float(data[data['Date'] == end_date]['Adj Close'])
+                year_normalized_price = end_adj_close/normalization_factor
+
+                result[ticker][year] = year_normalized_price
+            except:
+                result[ticker][year] = np.nan
+
+    return pd.DataFrame(result)
